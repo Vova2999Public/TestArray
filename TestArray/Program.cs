c3e842e287348704929af4d0a2de393068022558
Vova2999 <Vova2999@gmail.com>
2023-04-13 18:43:38 +0400

    Print array

--- TestArray/Program.cs
+++ TestArray/Program.cs
@@ -13,6 +13,10 @@ namespace TestArray
 			var array = Enumerable.Range(0, NumbersCount).Select(_ => Random.Next(10)).ToArray();
 
 			array = array.OrderBy(x => x).ToArray();
+
+			Console.WriteLine(string.Join(", ", array));
+
+			Console.ReadKey();
 		}
 	}
 }
