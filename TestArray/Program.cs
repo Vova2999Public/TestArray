109f84f042b37bde3be79f1e00594f37f43ea765
Vova2999 <Vova2999@gmail.com>
2023-04-13 19:42:00 +0400

    Random str item part

--- TestArray/Program.cs
+++ TestArray/Program.cs
@@ -7,12 +7,18 @@ namespace TestArray
 	public static class Program
 	{
 		private const int NumbersCount = 20;
+		private const int ValueLengthStart = 1;
+		private const int ValueLengthEnd = 5;
 		private static readonly Random Random = new();
 
 		public static void Main()
 		{
 			var array = Enumerable.Range(0, NumbersCount)
-				.Select(_ => new Item { Number = Random.Next(10) })
+				.Select(_ => new Item
+				{
+					Number = Random.Next(10),
+					Value = new string('x', Random.Next(ValueLengthStart, ValueLengthEnd + 1))
+				})
 				.ToArray();
 
 			array = array.OrderBy(x => x.Number).ToArray();
