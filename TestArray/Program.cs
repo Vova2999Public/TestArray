d164e4052eb28ef3f20d0a31b0b61bdc9a0aac8c
Vova2999 <Vova2999@gmail.com>
2023-04-13 18:59:06 +0400

    Change ranges

--- TestArray/Program.cs
+++ TestArray/Program.cs
@@ -6,11 +6,15 @@ namespace TestArray
 	public static class Program
 	{
 		private const int NumbersCount = 20;
+		private const int NumbersStart = 0;
+		private const int NumbersEnd = 99;
 		private static readonly Random Random = new();
 
 		public static void Main()
 		{
-			var array = Enumerable.Range(0, NumbersCount).Select(_ => Random.Next(10)).ToArray();
+			var array = Enumerable.Range(0, NumbersCount)
+				.Select(_ => Random.Next(NumbersStart, NumbersEnd + 1))
+				.ToArray();
 
 			array = array.OrderBy(x => x).ToArray();
 
